92ad64ec1bd67d7f75c8d2ec3773cb22a5a2408a
Cecilierc <eaa24crch@students.eaaa.dk>
2025-05-12 18:41:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//Til index
 document.getElementById('startOverlay').addEventListener('click', function () {
     window.location.href = "intro.html";
 });
+
+//Til intro
+function hideOverlay() {
+    document.getElementById("startOverlay").style.display = "none";
+}
+
+function toggleSound() {
+    alert("Lyd slået til/fra (du kan erstatte dette med rigtig lyd senere)");
+}
