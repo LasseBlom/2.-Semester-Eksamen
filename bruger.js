8a03300475091ee95412b225277aca5e6c04645d
JulieWijva <juliemeldgaard@hotmail.dk>
2025-05-13 12:48:55 +0200

    Add files via upload

--- bruger.js
+++ bruger.js
@@ -1,13 +1,15 @@
-// Lydikon
-function toggleSound() {
-    alert("Lyd slået til/fra (du kan erstatte dette med rigtig lyd senere)");
+// Afspil oplæsning
+const introLyd = new Audio('sounds/navn.m4a');
+
+// Afspiller brugerlyden
+function afspilBrugerlyd() {
+    introLyd.play();
 }
 
 // Viser "START SPIL"-knappen når begge navnefelter er udfyldt
 const spiller1 = document.getElementById("spiller1");
 const spiller2 = document.getElementById("spiller2");
 const startKnap = document.getElementById("startSpilKnap");
-const spiller2Hint = document.getElementById("spiller2Hint");
 
 function tjekNavne() {
     const navn1 = spiller1.value.trim();
@@ -15,24 +17,30 @@ function tjekNavne() {
 
     if (navn1 && navn2) {
         startKnap.style.display = "block";
-        spiller2.placeholder = ""; // Fjern placeholder hvis begge har skrevet
+        spiller2.placeholder = ""; //Fjern placeholder hvis begge navne er skrevet
     } else {
         startKnap.style.display = "none";
 
-        // Hvis spiller 1 har skrevet, men spiller 2 ikke har
+        //Hvis spiller 1 har skrvete navn, men spiller 2 ikke har
         if (navn1 && !navn2) {
             spiller2.placeholder = "Husk at skrive navn her";
         } else {
-            spiller2.placeholder = ""; // Fjern placeholder hvis ikke relevant
+            spiller2.placeholder = ""; //Fjern placeholder, hvis det ike er relevant
         }
     }
 }
 
-// Når brugeren skriver i ét af felterne, tjek om begge er udfyldt
+//Når brugeren skriver i ét af felterne - tjek at begge er udfyldt
 spiller1.addEventListener("input", tjekNavne);
 spiller2.addEventListener("input", tjekNavne);
 
-// Når der trykkes på knappen, gå til næste side
+// Når der trykkes på knappen, gem navne og gå til næste side
 function startSpil() {
+    const navn1 = spiller1.value.trim();
+    const navn2 = spiller2.value.trim();
+
+    localStorage.setItem("spiller1", navn1);
+    localStorage.setItem("spiller2", navn2);
+
     window.location.href = "vendespil.html";
 }
