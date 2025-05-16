88a76f184f4b5b1104c2af10c9cdb81620ac3015
Cecilierc <eaa24crch@students.eaaa.dk>
2025-05-16 08:31:28 +0200

    Add files via upload

--- kort.js
+++ kort.js
@@ -1,29 +1,31 @@
-const kort1 = new Audio('sounds/5til10.m4a');
 // Afspiller kort 1.1
+const kort1 = new Audio('sounds/5til10.m4a');
 function afspil5til10() {
     kort1.play();
 }
 
-const kort2 = new Audio('sounds/urolig.m4a');
 // Afspiller kort 2.1
+const kort2 = new Audio('sounds/urolig.m4a');
 function afspilurolig() {
     kort2.play();
 }
 
-const korthjerte = new Audio('sounds/hjerte.mp3');
 // Afspiller kort 2.2
+//Når man trykke på højtaleren afspilles lyden - det er ikke muligt,
+//at få den til at afspille automatisk grundet browsere blokerer automatisk lyd
+const korthjerte = new Audio('sounds/hjerte.mp3');
 function afspilhjerte() {
     korthjerte.play();
 }
 
-const kort3 = new Audio('sounds/bange.m4a');
 // Afspiller kort 3.1
+const kort3 = new Audio('sounds/bange.m4a');
 function afspilbange() {
     kort3.play();
 }
 
-const kort4 = new Audio('sounds/symptomer.m4a');
 // Afspiller kort 4.1
+const kort4 = new Audio('sounds/symptomer.m4a');
 function afspilsymptomer() {
     kort4.play();
 }
